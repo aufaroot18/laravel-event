80d8fbd10b958dbae743170843723b254c7f1317
Aufa Billah <aufaroot18@gmail.com>
2019-05-29 16:47:27 +0700

    Event Controller: Create createEvent and storeEvent method

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
 
-class EventController extends Controller
-{
-    //
+class EventController extends Controller {
+	public function index() {
+		return "All Event";
+	}
+	
+    public function createEvent() {
+    	return view('event.create');
+    }
+
+    public function storeEvent(Request $request) {
+    	$validateData = $request->validate([
+    		'nama' => 'required',
+    		'alamat' => 'required',
+    		'deskripsi' => 'required',
+    	]);
+
+    	Event::create([
+    		'nama' => $request->nama,
+    		'alamat' => $request->alamat,
+    		'deskripsi' => $request->deskripsi,
+    		'user_id' => $request->user()->id,
+    	]);
+
+    	return redirect()->action('EventController@createEvent')->with('status', 'Event Created');
+    }
 }
