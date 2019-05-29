2bd2f51f1e4ca3d188d034eb5e254351baa84e5f
Aufa Billah <aufaroot18@gmail.com>
2019-05-29 16:45:55 +0700

    Event Model: add fillable column

--- app/Event.php
+++ app/Event.php
@@ -8,4 +8,8 @@ class Event extends Model {
     public function user() {
     	return $this->belongsTo('App\User');
     }
+
+    protected $fillable = [
+    	'nama', 'alamat', 'deskripsi', 'user_id'
+    ];
 }
