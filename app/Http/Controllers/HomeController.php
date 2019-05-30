c3825a8ef0bcdc2ce033e761f88b3f93622e4733
Aufa Billah <aufaroot18@gmail.com>
2019-05-30 15:50:34 +0700

    Home Controller: order desc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Event;
+use DB;
 
 class HomeController extends Controller {	
     public function index() {
-    	$event = Event::all();
+    	// $event = Event::all();
+    	$event = DB::table('events')->orderBy('created_at', 'desc')->get();
     	return view('home', ['event' => $event]);
     }
 }
