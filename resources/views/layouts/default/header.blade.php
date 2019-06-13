d8b6f2c9ff8bad0bffab303154c81e35886c5950
Aufa Billah <aufaroot18@gmail.com>
2019-06-13 16:26:12 +0700

    Header View: remove header view in default layout

--- resources/views/layouts/default/header.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-4">Event Laravel</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, totam, dolore? Deleniti minus beatae, ducimus a dolorem nesciunt. Nihil eius autem aliquid consequatur aut, amet dolor sapiente earum cumque quidem.</p>
-        <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam iste labore qui animi dignissimos excepturi eaque corrupti. Repudiandae, dolorum in.</p>
-        <a href="#" class="btn btn-primary btn-lg">See Event</a>
-    </div>
-</div>
