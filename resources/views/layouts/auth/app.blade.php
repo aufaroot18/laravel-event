9078c38da7487a38a123b421bcd58b6109479014
Aufa Billah <aufaroot18@gmail.com>
2019-06-17 19:49:29 +0700

    Create layout for auth

---
+++ resources/views/layouts/auth/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <!-- Title -->
+    <title>Event Laravel</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <div id="app">
+        <!-- Navbar -->
+        @include('layouts.default.nav')
+        <!-- Navbar End -->
+
+        <!-- Main Content -->
+        <main>
+            @yield('content')
+        </main>
+        <!-- Main Content End -->
+    </div>
+
+    <!-- JavaScript -->
+    <!-- Jquery, Popper, Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+</body>
+</html>
