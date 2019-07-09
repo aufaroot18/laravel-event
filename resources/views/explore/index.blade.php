9fac8b424044b4299b8b32cc6614651fa83cf90d
Aufa Billah <aufaroot18@gmail.com>
2019-07-09 18:36:28 +0700

    home and explore view: add href for event detail

--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -10,9 +10,13 @@
                 @foreach($event as $e)
                     <div class="col-md-4 mb-4">
                         <div class="card h-100">
-                            <img src="{{url('gambar/'.$e->gambar)}}" alt="" class="card-img-top" height="200" width="300">
+                            <a href="{{route('explore.show', ['id' => $e->id])}}">
+                                <img src="{{url('gambar/'.$e->gambar)}}" alt="" class="card-img-top" height="200" width="300">
+                            </a>
                             <div class="card-body">
-                                <h5 class="card-title font-weight-bold">{{$e->nama}}</h5>
+                                <a href="{{route('explore.show', ['id' => $e->id])}}" class="text-dark text-decoration-none">
+                                    <h5 class="card-title font-weight-bold">{{$e->nama}}</h5>
+                                </a>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus ipsa temporibus ullam blanditiis</p>
                                 <p class="card-text">{{\Carbon\Carbon::parse($e->tanggal_mulai)->toFormattedDateString()}}</p>
                                 <p class="card-text">{{$e->alamat}}</p>
