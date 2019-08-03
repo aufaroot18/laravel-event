bee9690374b5e5d5f939629592d5f521708fe8cf
Aufa Billah <aufaroot18@gmail.com>
2019-08-03 14:34:57 +0700

    change footer content

--- resources/views/layouts/default/footer.blade.php
+++ resources/views/layouts/default/footer.blade.php
@@ -1,3 +1,15 @@
-<footer class="container">
-    <p>This is footer new</p>
+<footer>
+    <div class="container">
+    	<ul class="nav">
+    	  <li class="nav-item">
+    	    <a class="nav-link" href="#">Contact Us</a>
+    	  </li>
+    	  <li class="nav-item">
+    	    <a class="nav-link" href="#">About Us</a>
+    	  </li>
+    	  <li class="nav-item">
+    	    <a class="nav-link" href="#">Legal Information</a>
+    	  </li>
+    	</ul>
+    </div>
 </footer>
