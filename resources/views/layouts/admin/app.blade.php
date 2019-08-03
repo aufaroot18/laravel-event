93fe7e80d9cdde6aa6f5c76b6a2f56798dcd4ec8
Aufa Billah <aufaroot18@gmail.com>
2019-08-03 14:32:57 +0700

    change style, js sbadmin

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -10,9 +10,11 @@
   <meta name="author" content="">
 
   <title>SB Admin 2 - Dashboard</title>
+  
+  <!-- Icon -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-0c38nfCMzF8w8DBI+9nTWzApOpr1z0WuyswL4y6x/2ZTtmj/Ki5TedKeUcFusC/k" crossorigin="anonymous">
 
   <!-- Custom fonts for this template-->
-  <link href="{{asset('sbadmin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -86,21 +88,15 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="{{asset('sbadmin/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('sbadmin/js/jquery.min.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="{{asset('sbadmin/js/bootstrap.min.js')}}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="{{asset('sbadmin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('sbadmin/js/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="{{asset('sbadmin/js/sb-admin-2.min.js')}}"></script>
-
-  <!-- Page level plugins -->
-  <script src="{{asset('sbadmin/vendor/chart.js/Chart.min.js')}}"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="{{asset('sbadmin/js/demo/chart-area-demo.js')}}"></script>
-  <script src="{{asset('sbadmin/js/demo/chart-pie-demo.js')}}"></script>
 
   <!-- CKeditor -->
   <script src="https://cdn.ckeditor.com/4.11.4/standard/ckeditor.js"></script>
