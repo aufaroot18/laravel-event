f8c53879c5defd2c416f6ba49ca326e114286a1f
Aufa Billah <aufaroot18@gmail.com>
2019-08-05 21:40:08 +0700

    styling detail of event

--- resources/views/explore/show.blade.php
+++ resources/views/explore/show.blade.php
@@ -1,26 +1,32 @@
 @extends('layouts.default.app')
 
 @section('content')
-<main>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-7">
-				<img src="{{url('gambar/'.$event->gambar)}}" class="img-fluid rounded">
-			</div>
-			<div class="col-md-5">
-				<h1>{{$event->nama}}</h1>
-				<p>{{$event->alamat}}</p>
-				<p>
-					{{\Carbon\Carbon::parse($event->tanggal_mulai)->toFormattedDateString()}} - {{\Carbon\Carbon::parse($event->tanggal_akhir)->toFormattedDateString()}}
-				</p>
-				<p>{{$user->name}}</p>
-				<a href="#" class="btn btn-primary">Register</a>
+<main class="detail">
+	<div class="head">
+		<div class="container">
+			<div class="row head">
+				<div class="col-md-7">
+					<img src="{{url('gambar/'.$event->gambar)}}" class="img-fluid rounded">
+				</div>
+				<div class="col-md-5">
+					<h1>{{$event->nama}}</h1>
+					<p><i class="fas fa-map-marker-alt"></i>{{$event->alamat}}</p>
+					<p>
+						<i class="fas fa-calendar-week"></i>{{\Carbon\Carbon::parse($event->tanggal_mulai)->toFormattedDateString()}} - {{\Carbon\Carbon::parse($event->tanggal_akhir)->toFormattedDateString()}}
+					</p>
+					<p><i class="far fa-user"></i>Created by {{$user->name}}</p>
+					<a href="#" class="btn btn-primary">Join Event</a>
+				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-7">
-				<h3>Deskripsi Event</h3>
-				{!! $event->deskripsi !!}
+	</div>
+	<div class="body">
+		<div class="container">
+			<div class="row">
+				<div class="col-8">
+					<h3>Deskripsi</h3>
+					{!! $event->deskripsi !!}
+				</div>
 			</div>
 		</div>
 	</div>
