b29c579eac58840e6e85af108f2889676664ea57
Aufa Billah <aufaroot18@gmail.com>
2019-08-06 19:58:05 +0700

    create users_events migration

---
+++ database/migrations/2019_08_06_123946_create_users_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            // create foreging key
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('event_id');
+
+            // connect foreign key
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_events');
+    }
+}
