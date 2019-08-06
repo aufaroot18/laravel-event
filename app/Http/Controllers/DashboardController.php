de0c4d9dc6444a762f4c1065baf12e3bbef30fe5
Aufa Billah <aufaroot18@gmail.com>
2019-08-06 19:54:04 +0700

    Dashboard Controller: add list event to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
 
 class DashboardController extends Controller
 {
@@ -21,8 +22,10 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('dashboard');
+    public function index() {
+        $latestEvent = Event::orderBy('created_at', 'desc')->take(4)->get();
+        $closerEvent = Event::orderBy('nama', 'asc')->take(4)->get();
+        $event = array($latestEvent, $closerEvent);
+        return view('dashboard', ['event' => $event]);
     }
 }
