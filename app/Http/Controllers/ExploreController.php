d80209ad0b59a322b1405af03c877dd5b5962cf6
Aufa Billah <aufaroot18@gmail.com>
2019-08-06 20:00:30 +0700

    Explore event: add function show for detail event

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Event;
+use App\User;
 
 class ExploreController extends Controller {
     public function index() {
     	$event = Event::paginate(6);
     	return view('explore.index', ['event' => $event]);
+    }
+
+    public function show($id) {
+    	$event = Event::where('id', $id)->first();
+    	$user = User::where('id', $event->user_id)->first();
+    	return view('explore.show', ['event' => $event, 'user' => $user]);
     }
 }
