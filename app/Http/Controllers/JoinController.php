3f732951aaf5dfb2fe738609c548d5ef53753e9e
Aufa Billah <aufaroot18@gmail.com>
2019-08-07 16:06:21 +0700

    Join Controller: create function join

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\UserEvent;
 
-class JoinController extends Controller
-{
-    //
+class JoinController extends Controller {
+    public function join($id) {
+    	$user = Auth::user();
+    	$idEvent = $id;
+
+    	UserEvent::create([
+    		'user_id' => $user->id,
+    		'event_id' => $idEvent,
+    	]);
+
+    	return redirect()->action('ExploreController@show', ['id' => $idEvent]);
+    }
 }
