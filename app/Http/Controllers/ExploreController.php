082a635b74a9f6f70036a6df72cf979b183cfbbf
Aufa Billah <aufaroot18@gmail.com>
2019-08-07 16:11:56 +0700

    Explore Controller: add status join

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Event;
 use App\User;
+use App\UserEvent;
 
 class ExploreController extends Controller {
     public function index() {
@@ -13,8 +15,12 @@ class ExploreController extends Controller {
     }
 
     public function show($id) {
+    	// status join
+    	$statusJoin =  UserEvent::where('event_id', $id)->where('user_id', Auth::id())->first();
+
+    	// get event and creator of event
     	$event = Event::where('id', $id)->first();
     	$user = User::where('id', $event->user_id)->first();
-    	return view('explore.show', ['event' => $event, 'user' => $user]);
+    	return view('explore.show', ['event' => $event, 'user' => $user, 'statusJoin' => $statusJoin]);
     }
 }
