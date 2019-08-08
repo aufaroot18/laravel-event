2ec7c67324b196fac6146661534511ff59f80bcc
Aufa Billah <aufaroot18@gmail.com>
2019-08-08 23:00:44 +0700

    Check Event for this week, month, and all

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -4,13 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DateTime;
 use App\Event;
 use App\User;
 use App\UserEvent;
 
 class ExploreController extends Controller {
     public function index() {
-    	$event = Event::paginate(6);
+        // today
+        $from = date('Y-m-d');
+
+        // // get 1 week later
+        $to = new DateTime($from);
+        $to = $to->modify('+1 week');
+        $to = $to->format('Y-m-d');
+
+    	// $event = Event::paginate(6);
+        $event = Event::whereBetween('tanggal_mulai', [$from, $to])->paginate(6);
+
+
+        // get event this month if this week not exist
+        if (count($event) == 0) {
+            $event = Event::whereMonth('tanggal_mulai', date('m'))->paginate(6);
+            $status = "bulan ini";
+        }
+
+        // get all event if this month not exist
+        if (count($event) == 0) {
+            $event = Event::paginate(6);
+            $status = "semua";
+        }
+
     	return view('explore.index', ['event' => $event]);
     }
 
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Event;
+use DateTime;
 use DB;
 
 class HomeController extends Controller {	
     public function index() {
-    	// $event = Event::all();
-    	$event = DB::table('events')->orderBy('created_at', 'desc')->get();
+    	// today
+    	$from = date('Y-m-d');
+
+    	// // get 1 week later
+    	$to = new DateTime($from);
+    	$to = $to->modify('+1 week');
+    	$to = $to->format('Y-m-d');
+
+    	// get event this week
+    	$event = Event::whereBetween('tanggal_mulai', [$from, $to])->take(6)->get();
+    	$status = "minggu ini";
+
+    	// get event this month if this week not exist
+    	if (count($event) == 0) {
+    		$event = Event::whereMonth('tanggal_mulai', date('m'))->take(6)->get();
+    		$status = "bulan ini";
+    	}
+
+    	// get all event if this month not exist
+    	if (count($event) == 0) {
+    		$event = Event::take(6)->get();
+    		$status = "semua";
+    	}
+
     	return view('home', ['event' => $event]);
     }
 }
