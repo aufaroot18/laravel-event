750fd3f1698ffac1d9e2bae90a45d3625957dffb
Aufa Billah <aufaroot18@gmail.com>
2019-08-09 19:56:31 +0700

    Dashboard Controller: get event 1 week later, latest event, my event, and my event joined

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DateTime;
 use App\Event;
+use App\User;
+use App\UserEvent;
 
 class DashboardController extends Controller
 {
@@ -22,10 +25,41 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index() {
+    public function index(Request $request) {
+        // today
+        $from = date('Y-m-d');
+
+        // get 1 week later
+        $to = new DateTime($from);
+        $to = $to->modify('+1 week');
+        $to = $to->format('Y-m-d');
+
+        // get event this week
+        $eventThisWeek = Event::whereBetween('tanggal_mulai', [$from, $to])->paginate(4);
+
+        // get event this month if this week not exist
+        if (count($eventThisWeek) == 0) {
+            $eventThisWeek = Event::whereMonth('tanggal_mulai', date('m'))->paginate(4);
+            $status = "bulan ini";
+        }
+
+        // get all event if this month not exist
+        if (count($eventThisWeek) == 0) {
+            $eventThisWeek = Event::paginate(4);
+            $status = "semua";
+        }
+
+        // get latest or new event
         $latestEvent = Event::orderBy('created_at', 'desc')->take(4)->get();
-        $closerEvent = Event::orderBy('nama', 'asc')->take(4)->get();
-        $event = array($latestEvent, $closerEvent);
+
+        // get My Event
+        $idUser = $request->user()->id;
+        $myEvent = count(User::find($idUser)->events);
+
+        // get Event was joined
+        $myEventJoined = count(UserEvent::where('user_id', $idUser)->get());
+
+        $event = array($eventThisWeek, $latestEvent, $myEvent, $myEventJoined);
         return view('dashboard', ['event' => $event]);
     }
 }
