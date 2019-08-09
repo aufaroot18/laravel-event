6fc78636d88aab3d5b2bfec5df07edb1c928cc0d
Aufa Billah <aufaroot18@gmail.com>
2019-08-09 14:15:47 +0700

    Explore Controller: add function search, week, month, and all

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -19,9 +19,8 @@ class ExploreController extends Controller {
         $to = $to->modify('+1 week');
         $to = $to->format('Y-m-d');
 
-    	// $event = Event::paginate(6);
+        // get this week
         $event = Event::whereBetween('tanggal_mulai', [$from, $to])->paginate(6);
-
 
         // get event this month if this week not exist
         if (count($event) == 0) {
@@ -38,6 +37,70 @@ class ExploreController extends Controller {
     	return view('explore.index', ['event' => $event]);
     }
 
+    public function search(Request $request) {
+        $searchEvent = $request->input('search');
+        $event = Event::where('nama', 'like', '%'.$searchEvent.'%' )->paginate(6);
+        
+        return view('explore.index', ['event' => $event]);
+    }
+
+    // get event this week
+    public function week() {
+        // statusDropdown
+        $statusDropdown = 'This Week';
+
+        // today
+        $from = date('Y-m-d');
+
+        // // get 1 week later
+        $to = new DateTime($from);
+        $to = $to->modify('+1 week');
+        $to = $to->format('Y-m-d');
+
+        // get this week
+        $event = Event::whereBetween('tanggal_mulai', [$from, $to])->paginate(6);
+
+        // get event this month if this week not exist
+        if (count($event) == 0) {
+            $event = Event::whereMonth('tanggal_mulai', date('m'))->paginate(6);
+            $status = "bulan ini";
+        }
+
+        // get all event if this month not exist
+        if (count($event) == 0) {
+            $event = Event::paginate(6);
+            $status = "semua";
+        }
+
+        return view('explore.index', ['event' => $event, 'statusDropdown' => $statusDropdown]);
+    }
+
+    public function month() {
+        $statusDropdown = 'This Month';
+
+        // get event this month
+        $event = Event::whereMonth('tanggal_mulai', date('m'))->paginate(6);
+        $status = "bulan ini";
+
+        // get all event if this month not exist
+        if (count($event) == 0) {
+            $event = Event::paginate(6);
+            $status = "semua";
+        }
+
+        return view('explore.index', ['event' => $event, 'statusDropdown' => $statusDropdown]);
+    }
+
+    public function all() {
+        $statusDropdown = 'This Month';
+        
+        // get all event
+        $event = Event::paginate(6);
+        $status = "semua";
+
+        return view('explore.index', ['event' => $event, 'statusDropdown' => $statusDropdown]);
+    }
+
     public function show($id) {
     	// status join
     	$statusJoin =  UserEvent::where('event_id', $id)->where('user_id', Auth::id())->first();
@@ -47,4 +110,5 @@ class ExploreController extends Controller {
     	$user = User::where('id', $event->user_id)->first();
     	return view('explore.show', ['event' => $event, 'user' => $user, 'statusJoin' => $statusJoin]);
     }
+
 }
