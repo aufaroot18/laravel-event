1c69348d6cf2fb43c1cc329db5cc0e66656c4aa7
Aufa Billah <aufaroot18@gmail.com>
2019-08-09 19:58:31 +0700

    Dashboard View: show event 1 week later, latest event, my event, and event joined

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,20 +5,24 @@
 <div class="container-fluid">
 
   <!-- Page Heading -->
-  <h1 class="h3 mb-4 text-gray-800">Event Terdekat Minggu ini</h1>
+  <h1 class="h3 mb-4 text-gray-800">Event Terdekat</h1>
 
   <div class="row">
-  	@foreach($event[1] as $e)
+  	@foreach($event[0] as $e)
       <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-danger shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-danger text-uppercase mb-3">{{$e->nama}}</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+              <div class="col">
+                <div class="font-weight-bold text-uppercase mb-2">
+                  <a class="text-danger text-decoration-none" href="{{route('explore.show', ['id' => $e->id])}}">{{$e->nama}}</a>
+                </div>
+                <div class="mb-2">
+                  <i class="fas fa-calendar-week mr-2"></i>{{\Carbon\Carbon::parse($e->tanggal_mulai)->format('M d Y, H:i')}}
+                </div>
+                <div class="mb-1">
+                  <i class="fas fa-map-marker-alt mr-2"></i>{{$e->alamat}}
+                </div>
               </div>
             </div>
           </div>
@@ -30,17 +34,21 @@
   <h1 class="h3 mb-4 text-gray-800">Event Terbaru</h1>
 
   <div class="row">
-    @foreach($event[0] as $e)
+    @foreach($event[1] as $e)
       <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-primary text-uppercase mb-3">{{$e->nama}}</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                <div class="font-weight-bold text-primary text-uppercase mb-3">
+                  <a class="text-primary text-decoration-none" href="{{route('explore.show', ['id' => $e->id])}}">{{$e->nama}}</a>
+                </div>
+                <div class="mb-2">
+                  <i class="fas fa-calendar-week mr-2"></i>{{\Carbon\Carbon::parse($e->tanggal_mulai)->format('M d Y, H:i')}}
+                </div>
+                <div class="mb-1">
+                  <i class="fas fa-map-marker-alt mr-2"></i>{{$e->alamat}}
+                </div>
               </div>
             </div>
           </div>
@@ -57,8 +65,13 @@
   	    <div class="card-body">
   	      <div class="row no-gutters align-items-center">
   	        <div class="col mr-2">
-  	          <div class="font-weight-bold text-primary text-uppercase mb-3">My Events</div>
-  	          <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+  	          <div class="font-weight-bold text-primary text-uppercase mb-3">
+               <a class="text-primary text-decoration-none" href="{{route('dashboard.event')}}">Events Was Created</a> 
+              </div>
+              <!-- 
+                Get My Event
+               -->
+  	          <div class="h5 mb-0 font-weight-bold text-gray-800">{{$event[2]}} Events</div>
   	        </div>
   	        <div class="col-auto">
   	          <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -73,8 +86,13 @@
   	    <div class="card-body">
   	      <div class="row no-gutters align-items-center">
   	        <div class="col mr-2">
-  	          <div class="font-weight-bold text-success text-uppercase mb-3">My Tickets</div>
-  	          <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+  	          <div class="font-weight-bold text-success text-uppercase mb-3">
+               <a class="text-success text-decoration-none" href="#">Events Was Joined</a> 
+              </div>
+              <!-- 
+                Get My Event Joined
+               -->
+  	          <div class="h5 mb-0 font-weight-bold text-gray-800">{{$event[3]}} Events</div>
   	        </div>
   	        <div class="col-auto">
   	          <i class="fas fa-calendar fa-2x text-gray-300"></i>
