99c49d2879489276cbf7da4ee1c686ebe43ab0b3
Aufa Billah <aufaroot18@gmail.com>
2019-08-10 15:16:25 +0700

    Ticket index view: create view ticket

---
+++ resources/views/ticket/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin.app')
+
+@section('content')
+<div class="container-fluid">
+	<h1 class="h3 mb-4 text-gray-800">My Event</h1>
+	<div class="row">
+		@foreach($myEventJoined as $m)
+			<div class="col-md-3 mb-4">
+				<div class="card h-100 border-bottom-primary">
+					<div class="card-body">
+						<h5 class="text-primary card-title font-weight-bold">
+							<a class="text-primary text-decoration-none" href="{{route('explore.show', ['id' => $m->event_id])}}">{{$m->nama}}</a>
+						</h5>
+						<p>
+						  <i class="fas fa-calendar-week mr-2"></i>{{\Carbon\Carbon::parse($m->tanggal_mulai)->format('M d Y, H:i')}}
+						</p>
+						<p>
+						  <i class="fas fa-map-marker-alt mr-2"></i>{{$m->alamat}}
+						<p>
+					</div>
+				</div>
+			</div>
+		@endforeach
+	</div>
+</div>
+@endsection
