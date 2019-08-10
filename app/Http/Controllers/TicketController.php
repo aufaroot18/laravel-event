82895f8e09e52c9e24374e15b6fc5fb1fc94b6bd
Aufa Billah <aufaroot18@gmail.com>
2019-08-10 15:16:06 +0700

    Ticket Controller: create ticket or event joined

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UserEvent;
 
-class TicketController extends Controller
-{
-    //
+class TicketController extends Controller {
+    public function index(Request $request) {
+    	$idUser = $request->user()->id;
+    	// get my event joined
+    	// $myEventJoined = UserEvent::where('users_events.user_id', $idUser)->join('events', 'event_id', '=', 'events.id')->get();
+    	$myEventJoined = UserEvent::where('users_events.user_id', $idUser)->leftJoin('events', 'users_events.event_id', '=', 'events.id')
+    	->select('users_events.id', 'users_events.created_at', 'users_events.user_id', 'events.id as event_id', 'events.nama', 'events.alamat', 'events.deskripsi', 'events.gambar', 'events.tanggal_mulai', 'events.tanggal_selesai')
+    	->get();
+    	
+    	return view('ticket.index', ['myEventJoined' => $myEventJoined]);
+    }
 }
