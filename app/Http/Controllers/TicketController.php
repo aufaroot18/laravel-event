7f26d7bbd72ff3dc9d65a64415b611ab67822190
Aufa Billah <aufaroot18@gmail.com>
2019-08-11 12:31:44 +0700

    Ticket Conttroller: add function pdf

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,16 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\UserEvent;
+use PDF;
 
 class TicketController extends Controller {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request) {
     	$idUser = $request->user()->id;
-    	// get my event joined
-    	// $myEventJoined = UserEvent::where('users_events.user_id', $idUser)->join('events', 'event_id', '=', 'events.id')->get();
+
+    	// get all my event joined
     	$myEventJoined = UserEvent::where('users_events.user_id', $idUser)->leftJoin('events', 'users_events.event_id', '=', 'events.id')
-    	->select('users_events.id', 'users_events.created_at', 'users_events.user_id', 'events.id as event_id', 'events.nama', 'events.alamat', 'events.deskripsi', 'events.gambar', 'events.tanggal_mulai', 'events.tanggal_selesai')
-    	->get();
+    	->select('users_events.id', 'users_events.created_at', 'users_events.user_id', 'events.id as event_id', 'events.nama', 'events.alamat', 'events.deskripsi', 'events.gambar', 'events.tanggal_mulai', 'events.tanggal_selesai')->get();
     	
     	return view('ticket.index', ['myEventJoined' => $myEventJoined]);
+    }
+
+    public function pdf(Request $request, $id) {
+        $user = $request->user();
+        $idUser = $request->user()->id;
+        $idEvent = $id;
+        
+        // get my event joined, one event
+        $myEventJoined = UserEvent::where('users_events.user_id', $idUser)->where('users_events.event_id', $idEvent)->leftJoin('events', 'users_events.event_id', '=', 'events.id') ->select('users_events.id', 'users_events.created_at', 'users_events.user_id', 'events.id as event_id', 'events.nama', 'events.alamat', 'events.deskripsi', 'events.gambar', 'events.tanggal_mulai', 'events.tanggal_selesai') ->get();
+
+        // print to pdf
+        if ($myEventJoined == '[]') {
+            return abort(404);
+        }
+        else {
+            $pdf = PDF::loadView('ticket.pdf', ['myEventJoined' => $myEventJoined, 'user' => $user]);
+            return $pdf->stream();
+        }
     }
 }
